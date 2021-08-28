ec555c5e1154befc80f875aacc8cd3861879d060
cbrwx <81207429+cbrwx@users.noreply.github.com>
2021-08-28 02:03:13 +0200

    Update CryptoNoteWallet.cmake

--- CryptoNoteWallet.cmake
+++ CryptoNoteWallet.cmake
@@ -1,4 +1,4 @@
 
-set(CN_PROJECT_NAME "FairlightCoin")
-set(CN_CURRENCY_DISPLAY_NAME "FairlightCoin")
+set(CN_PROJECT_NAME "Fairlightcoin")
+set(CN_CURRENCY_DISPLAY_NAME "Fairlightcoin")
 set(CN_CURRENCY_TICKER "FLC")
